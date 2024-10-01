d239341615e58f3f9c83a8450102bcad67577575
Md. Mansur <ahammadullah2001@gmail.com>
2024-10-01 17:56:28 +0600

    scroll back to top

--- src/components/ui/ScrollToTop/ScrollToTop.tsx
+++ src/components/ui/ScrollToTop/ScrollToTop.tsx
@@ -1,42 +1,42 @@
-import { useState, useEffect } from "react";
-import { BiUpArrow } from "react-icons/bi";
-import { MdKeyboardArrowUp } from "react-icons/md";
+// import { useState, useEffect } from "react";
+// import { BiUpArrow } from "react-icons/bi";
+// import { MdKeyboardArrowUp } from "react-icons/md";
 
-const ScrollToTopButton = () => {
-  const [showButton, setShowButton] = useState(false);
+// const ScrollToTopButton = () => {
+//   const [showButton, setShowButton] = useState(false);
 
-  useEffect(() => {
-    const handleScroll = () => {
-      if (window.scrollY > 20) {
-        setShowButton(true);
-      } else {
-        setShowButton(false);
-      }
-    };
+//   useEffect(() => {
+//     const handleScroll = () => {
+//       if (window.scrollY > 20) {
+//         setShowButton(true);
+//       } else {
+//         setShowButton(false);
+//       }
+//     };
 
-    window.addEventListener("scroll", handleScroll);
-    return () => window.removeEventListener("scroll", handleScroll);
-  }, []);
+//     window.addEventListener("scroll", handleScroll);
+//     return () => window.removeEventListener("scroll", handleScroll);
+//   }, []);
 
-  const scrollToTop = () => {
-    window.scrollTo({
-      top: 0,
-      behavior: "smooth",
-    });
-  };
+//   const scrollToTop = () => {
+//     window.scrollTo({
+//       top: 0,
+//       behavior: "smooth",
+//     });
+//   };
 
-  return (
-    <>
-      {showButton && (
-        <button
-          onClick={scrollToTop}
-          className="fixed bottom-4 right-4 p-3 bg-slate-300 text-white rounded-full shadow-lg hover:bg-slate-500 transition-colors duration-300"
-          aria-label="Scroll to Top">
-          <MdKeyboardArrowUp />
-        </button>
-      )}
-    </>
-  );
-};
+//   return (
+//     <>
+//       {showButton && (
+//         <button
+//           onClick={scrollToTop}
+//           className="fixed bottom-4 right-4 p-3 bg-slate-300 text-white rounded-full shadow-lg hover:bg-slate-500 transition-colors duration-300"
+//           aria-label="Scroll to Top">
+//           <MdKeyboardArrowUp />
+//         </button>
+//       )}
+//     </>
+//   );
+// };
 
-export default ScrollToTopButton;
+// export default ScrollToTopButton;
